f2542a8d088351c4e7deb7e82cc86ee5cfad6475
Anna Zankevich <annzankevich18@gmail.com>
2019-05-19 22:19:32 +0300

    complete task

--- index.js
+++ index.js
@@ -6,46 +6,52 @@ window.onload = async () => {
     <button type="button" form="data" class="button">Search</button>
   </form>
   <div id="video-container"></div>
+  <div id="pagination"></div>
 `;
     document.querySelector('body').appendChild(head);
-    document.querySelector('form#data button').addEventListener('click', search );
-    document.querySelector('form#data #query-input').addEventListener('keyup', doSearch );
+    document.querySelector('form#data button').addEventListener('click', search);
+    document.querySelector('form#data #query-input').addEventListener('keyup', doSearch);
 };
-async function search () {
+
+async function search() {
     let query = document.querySelector('#query-input').value;
     await youtubeSearch(query);
 }
 
 this.delayTimer = null;
+
 function doSearch() {
     clearTimeout(this.delayTimer);
-    this.delayTimer = setTimeout(function() {
+    this.delayTimer = setTimeout(function () {
         search();
     }, 1000);
 }
 
+let videoList = [];
+
 async function youtubeSearch(query) {
-    const key = 'AIzaSyDPHc-_wuKwo-WuSN2CIEo8lGSBzVrENng';
+    const key = 'AIzaSyB9y6D3-w8Pf-oqCQ1B_buoFyEvJ7nzfp8';
     const url = 'https://www.googleapis.com/youtube/v3/search';
     const tail = 'type=video&part=snippet&maxResults=15';
     const result = await fetch(`${url}?key=${key}&${tail}&q=${query}${this.createPageTokenString()}`);
     const data = await result.json();
     this.nextPageToken = data.nextPageToken;
-    const items = data.items;
-
+    videoList = data.items;
     //this.ids = this.ids.concat(items.map(elem => elem.id.videoId));
     //return this.ids;
-    createDivs(items);
+    createDivs(videoList, 0);
 }
 
 
-function createPageTokenString(){
-return this.nextPageToken ? `&pageToken=${this.nextPageToken}` : '';
+function createPageTokenString() {
+    return this.nextPageToken ? `&pageToken=${this.nextPageToken}` : '';
+
 }
 
-function createDivs(videos) {
+function createDivs(videos, indexActive) {
     document.querySelector('#video-container').innerHTML = '';
-    videos.forEach(video=>{
+    const visibleVideos = videos.slice(indexActive * 4, indexActive * 4 + 4);
+    visibleVideos.forEach(video => {
         let videoSnippet = video.snippet;
         const div = document.createElement('div');
         div.innerHTML = `
@@ -55,13 +61,41 @@ function createDivs(videos) {
               <div class="title">
                 <a href="https://www.youtube.com/watch?v=${video.id.videoId}">${videoSnippet.title}</a>
               </div>
-              <div class="about">
-                <p><i class="fa fa-user" aria-hidden="true"></i><span id="author">${videoSnippet.channelTitle}</span></p>
-                <p><i class="fa fa-calendar" aria-hidden="true"></i><span id="date">${videoSnippet.publishedAt}</span></p>
-                <p><i class="fa fa-eye" aria-hidden="true"></i><span id="views">${video.viewCount}</span></p>      
+              <div class="video-link">
+                <p><i class="description" aria-hidden="true"></i><span id="author">${videoSnippet.channelTitle}</span></p>
+                <p><i class="description" aria-hidden="true"></i><span id="date">${videoSnippet.publishedAt.substring(0, 10)}</span></p>
+                <p><i class="description" aria-hidden="true"></i><span id="views">${video.viewCount}</span></p>      
                 <p class="description">${videoSnippet.description}</p>
               </div>`;
         div.classList.add('item');
         document.querySelector('#video-container').appendChild(div);
-    })
+    });
+    createPagination(4, 0);
+}
+
+
+function createPagination(count, activeItem) {
+    document.querySelector('#pagination').innerHTML = '';
+    const pagination = document.getElementById('pagination');
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('div');
+        item.classList.add('page-item');
+        if (i === activeItem) {
+            item.classList.add('active');
+        }
+        pagination.appendChild(item);
+    }
+    pagination.addEventListener('click', handleActiveItem);
+
+}
+
+function handleActiveItem() {
+    if (event.target.classList.contains('page-item')) {
+        const pagination = document.getElementById('pagination');
+        [...pagination.children].forEach(item => item.classList.remove("active"));
+        event.target.classList.add('active');
+        let index = [...pagination.children].findIndex( item => item.classList.contains('active'));
+        console.log(index);
+        createDivs(videoList,index);
+    }
 }
