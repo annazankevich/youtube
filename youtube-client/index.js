26503ea241c5e73e988c0b05374779f94fee8c18
Anna Zankevich <annzankevich18@gmail.com>
2019-08-10 19:04:17 +0300

    add project

---
+++ youtube-client/index.js
@@ -0,0 +1,106 @@
+window.onload = async () => {
+  const head = document.createElement('header');
+  head.innerHTML = `
+  <form id="data">
+    <input type="text" id="query-input" placeholder="Find clip name">
+    <button type="button" form="data" class="button">Search</button>
+  </form>
+  <div id="video-container"></div>
+  <div id="pagination"></div>
+`;
+  document.querySelector('body').appendChild(head);
+  document.querySelector('form#data button').addEventListener('click', search);
+  document.querySelector('form#data #query-input').addEventListener('keyup', doSearch);
+};
+
+async function search() {
+  let query = document.querySelector('#query-input').value;
+  await youtubeSearch(query);
+}
+
+this.delayTimer = null;
+
+function doSearch() {
+  clearTimeout(this.delayTimer);
+  this.delayTimer = setTimeout(function () {
+    search();
+  }, 1000);
+}
+
+let videoList = [];
+
+async function youtubeSearch(query) {
+  const key = 'AIzaSyDPHc-_wuKwo-WuSN2CIEo8lGSBzVrENng';
+  const url = 'https://www.googleapis.com/youtube/v3/search';
+  const tail = 'type=video&part=snippet&maxResults=15';
+  const result = await fetch(`${url}?key=${key}&${tail}&q=${query}${this.createPageTokenString()}`);
+  const data = await result.json();
+  this.nextPageToken = data.nextPageToken;
+  videoList = data.items;
+  createDivs(videoList, 0);
+}
+
+
+function createPageTokenString() {
+  return this.nextPageToken ? `&pageToken=${this.nextPageToken}` : '';
+
+}
+
+function createPageTokenString() {
+  return this.nextPageToken ? `&pageToken=${this.nextPageToken}` : '';
+
+}
+
+function createDivs(videos, indexActive) {
+  document.querySelector('#video-container').innerHTML = '';
+  const visibleVideos = videos.slice(indexActive * 4, indexActive * 4 + 4);
+  visibleVideos.forEach(video => {
+    let videoSnippet = video.snippet;
+    const div = document.createElement('div');
+    div.innerHTML = `
+              <div class="picture">
+                <img src=${video.snippet.thumbnails.medium.url} alt="img">
+              </div>
+              <div class="title">
+                <a href="https://www.youtube.com/watch?v=${video.id.videoId}">${videoSnippet.title}</a>
+              </div>
+              <div class="video-link">
+                <p><i class="description" aria-hidden="true"></i><span id="author">${videoSnippet.channelTitle}</span></p>
+                <p><i class="description" aria-hidden="true"></i><span id="date">${videoSnippet.publishedAt.substring(0, 10)}</span></p>
+                <p><i class="description" aria-hidden="true"></i><span id="views">${video.viewCount}</span></p>      
+                <p class="description">${videoSnippet.description}</p>
+              </div>`;
+    div.classList.add('item');
+    document.querySelector('#video-container').appendChild(div);
+  });
+  createPagination(4, 0);
+}
+
+
+function createPagination(count, activeItem) {
+  document.querySelector('#pagination').innerHTML = '';
+  const pagination = document.getElementById('pagination');
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('div');
+    item.classList.add('page-item');
+    if (i === activeItem) {
+      item.classList.add('active');
+    }
+    pagination.appendChild(item);
+  }
+  pagination.addEventListener('click', handleActiveItem, false);
+
+}
+
+function handleActiveItem() {
+  if (event.target.classList.contains('page-item')) {
+    const pagination = document.getElementById('pagination');
+    [...pagination.children].forEach(item => item.classList.remove("active"));
+    event.target.classList.add('active');
+    let p = event.target.parentElement;
+    let index = Array.prototype.indexOf.call(p.children, event.target);
+    console.log(index);
+    createDivs(videoList, index);
+  }
+}
+
